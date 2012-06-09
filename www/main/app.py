b67bca6e36fc0264fe58bd14469496ec9fe77483
TimGLuz <timgluz@gmail.com>
2012-06-09 17:36:32 +0300

    added Facebook login

--- www/main/app.py
+++ www/main/app.py
@@ -134,6 +134,21 @@ def data_demo():
         "val":  random.randint(1, 20)}
     return jsonify(arr)
 
+@main.route("/channel", methods = ["GET"])
+def channel():
+    '''
+    servers channel file for FB JS sdk.
+    read more: https://developers.facebook.com/docs/reference/javascript/
+    '''
+    cache_expire = 60 * 60 * 24 * 365
+    expiration_date = datetime.utcnow() + timedelta(seconds = cache_expire)
+    response = make_response(render_template("channel.html"))
+    response.headers.update({
+        "Pragma" : "public",
+        "Cache-Control" : "max-age={0}".format(cache_expire),
+        "Expires" : expiration_date.strptime("%a, %d %m %Y %H:%M:%S")
+    })
+    return response
 
 @main.route("/login", methods = ["POST"])
 def login():
@@ -144,28 +159,18 @@ def login():
     user_info = mjson.loads(request.data)
     print user_info
     if 'user_id' not in session:
-        user = g.db.users.User.find_one({"social.id": user_info["id"]});
-
+        print "registering new user"
+        user = g.db.users.find_one({"id": user_info["id"]});
         if user is None:
             #if we didnt find such user, then signup as new user.
-            user = g.db.users.User()
-            user.update({
+            user_id = g.db.users.insert({
+                "_id": user_info["id"],
                 "name" : user_info["name"],
                 "email" : user_info["email"],
-                "social" : {
-                    "link" : user_info["link"],
-                    "id" : user_info["id"],
-                }
+                "registered": datetime.utcnow()
             });
-            user.save()
-            register_action({
-                "activator" : user["_id"],
-                "action" : u"signup",
-                "subject" : user["_id"]
-            })
-
-
-        session["user_id"] = str(user["_id"])
+            
+        session["user_id"] = str(user_id)
 
     return success("Accepted. User session initialized.")
 
@@ -174,5 +179,5 @@ def login():
 def logout():
     if 'user_id' in session:
         del session["user_id"]
-
-    return success("User is logged out.");
+    #return success("User is logged out.");
+    return redirect('/')
---
+++ www/static/js/fb.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '411168845594539',//'422541527766916', // PROD
+      channelUrl : window.location.href + 'channel', // Channel File
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+
+    // Additional initialization code here
+  };
+
+  // Load the SDK Asynchronously
+  (function(d){
+     var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     d.getElementsByTagName('head')[0].appendChild(js);
+   }(document));
+
+
+   //add action to login button
+   $("#login-button").on("click", function(){
+       FB.login(function(response) {
+       if (response.authResponse) {
+         console.log('Welcome!  Fetching your information.... ');
+         FB.api('/me', function(response) {
+           console.log('Good to see you, ' + response.name + '.');
+           console.log(response)
+            //send user info to server
+            $.ajax({
+              url : "/login",
+              type : "POST",
+              contentType : "application/json",
+              dataType : "json",
+              data : JSON.stringify(response),
+              success : function(){window.location.replace(window.location.href + "balance");},
+            });
+           //FB.logout(function(response) { console.log('Logged out.');  });
+         });
+       } else {
+         console.log('User cancelled login or did not fully authorize.');
+         window.location.replace(window.location.href);
+       }
+     }, {scope: 'email'});
+
+   });
+});
